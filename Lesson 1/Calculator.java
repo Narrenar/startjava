3dcd0719d8ff2bdfc8f724ec67237405e11cf5be
Narrenar <georgemoney007@mail.ru>
2018-09-17 16:04:51 +0300

    Исправленное ДЗ 2.2

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -1,23 +1,24 @@
 public class Calculator {
 	public static void main(String[] args) {
 		int firstNumber = 42;
-		int secondNumber = 3;
-		String usersMathAction = "^";
+		int secondNumber = 4;
+		String mathSign = "^";
 
-		if (usersMathAction == "+") {
+		if (mathSign == "+") {
 			System.out.println(firstNumber + secondNumber);
-		} else if (usersMathAction == "-") {
+		} else if (mathSign == "-") {
 			System.out.println(firstNumber - secondNumber);
-		} else if (usersMathAction == "*") {
+		} else if (mathSign == "*") {
 			System.out.println(firstNumber * secondNumber);
-		} else if (usersMathAction == "/") {
+		} else if (mathSign == "/") {
 			System.out.println(firstNumber / secondNumber);
-		} else if (usersMathAction == "^") {
-			for (int i = secondNumber; i >= 1; i--) {
-				firstNumber = firstNumber * firstNumber;
+		} else if (mathSign == "^") {
+			int firstNumberInitialCopy = firstNumber;
+			for (int i = secondNumber; i >= 2; i--) {
+				firstNumber = firstNumber * firstNumberInitialCopy;
 			}
 			System.out.println(firstNumber);
-		} else if (usersMathAction == "%") {
+		} else if (mathSign == "%") {
 			System.out.println(firstNumber % secondNumber);
 		} else {
 			System.out.println("Wrong value. The Calculator accepts only these symbols: +, -, *, /, ^, %, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9");
--- Lesson 1/MyFirstGame.java
+++ Lesson 1/MyFirstGame.java
@@ -1,22 +1,19 @@
 public class MyFirstGame {
 	public static void main(String[] args) {
 		int hiddenNumber = 42;
-		int usersGuess = 100;
+		int userGuess = 100;
 
 		do {
-			if (usersGuess > hiddenNumber) {
+			if (userGuess > hiddenNumber) {
 				System.out.println("Введенное вами число больше того, что загадал компьютер");
-				usersGuess -= 1;
-			} else if (usersGuess < hiddenNumber) {
+				userGuess--;
+			} else if (userGuess < hiddenNumber) {
 				System.out.println("Введенное вами число меньше того, что загадал компьютер");
-				usersGuess += 1;
-			} else if (usersGuess == hiddenNumber) {
-				System.out.println("Вы угадали!");
+				userGuess++;
 			} else {
 				System.out.println("You have chosen the wrong value (like #)! You can use only Figures and Numbers with these symbols: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9");
 			}
-		} 
-		while (usersGuess != hiddenNumber);
-		System.out.println("Загаданное число: " + 42);
+		} while (userGuess != hiddenNumber);
+		System.out.println("Вы угадали! Загаданное число: " + 42);
 	}
 }
