f167abb98f07fd5d124b6bc2a815a57598f202b0
Narrenar <georgemoney007@mail.ru>
2018-12-12 13:05:59 +0300

    Сделал калькулятор Lesson 2

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -1,15 +1,3 @@
-/*
-1. Определить математическую оперецию, вводимые пользователем
-2. Определяет первое и второе число, вводимое пользователем
-*/
-
-/*
-Scaner scan = new Scanner(System.in);
-
-		int number1 = scan.int();
-		String mathSign = scan.next();
-		int number2 = scan.int();
-*/
 import java.util.Scanner;
 
 public class CalculatorTest {
@@ -28,15 +16,3 @@ public class CalculatorTest {
 		System.out.println("Answer: " + calculator1.mathSignCheck(0));
 	}
 }
-
-	/*	System.out.println("Choose the FIRST number");
-		int number1 = scan.nextInt ();
-		System.out.println("Choose the math OPERATION ");
-		String mathSign = scan.next();
-		System.out.println("Choose the SECOND number");
-		int number2 = scan.nextInt();*/
-
-		// Нужно использовать Calculator, чтобы вычислить знанения из инпутов
-		/*System.out.println(number1);
-		System.out.println(number2);
-		System.out.println(mathSign);*/
